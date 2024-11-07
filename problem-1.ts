a5109bfc4660d6343d48375fe017082b407d98d9
Rahim00001 <rahimlpt2022@gmail.com>
2024-11-07 14:06:37 +0600

    problem 1 solved

--- problem-1.ts
+++ problem-1.ts
@@ -1,1 +1,17 @@
-console.log("Typescript Assignment");
+{
+
+    function sumArray(numbers: number[]): number {
+        let sum = 0;
+        for (let i = 0; i < numbers.length; i++) {
+            sum += numbers[i]
+        }
+        return sum
+    }
+
+
+
+
+    // Test array
+    // const numbers = [1, 2, 3, 4, 5];
+    // console.log(sumArray(numbers));
+}
