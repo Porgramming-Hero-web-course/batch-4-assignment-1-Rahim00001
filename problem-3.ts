e6a88276de63cc0332440eeeeac8fd53671c6ac2
Rahim00001 <rahimlpt2022@gmail.com>
2024-11-07 22:39:46 +0600

    on previous push i completed problem-4 but I submitted as problem-3. Now I am submitting problem-3.

--- problem-3.ts
+++ problem-3.ts
@@ -1,30 +1,13 @@
 {
     // problem-3
 
-    type Circle = {
-        shapeType: "circle";
-        radius: number;
-    }
-    type Rectangle = {
-        shapeType: "rectangle";
-        height: number;
-        width: number;
-    }
-    type Shape = Circle | Rectangle;
-
-    function calculateShapeArea(shape: Shape): number {
-        if (shape.shapeType === "circle") {
-            const PiValue = Math.PI;
-            const circleRedius = shape.radius;
-            const rediusSqure = circleRedius * circleRedius;
-            return (PiValue * rediusSqure)
-        } else if (shape.shapeType === "rectangle") {
-            const height = shape.height;
-            const width = shape.width;
-            return (height * width)
-        } else {
-            throw new Error("Please insert valid data");
-        }
+    function countWordOccurrences(sentence: string, word: string): number {
+        const lowerSent = sentence.toLowerCase();
+        const lowerWord = word.toLowerCase();
+        const allWords = lowerSent.split(' ');
+        const matchedWord = allWords.filter(word => word === lowerWord);
+        const matchCount = matchedWord.length;
+        return matchCount;
     }
 
 
@@ -35,17 +18,10 @@
 
 
 
-    // Sample Input 1:
-    // const circleArea = calculateShapeArea({ shapeType: "circle", radius: 5 });
 
-    // Sample Input 2:
-    // const rectangleArea = calculateShapeArea({
-    //     shapeType: "rectangle",
-    //     width: 4,
-    //     height: 6,
-    // });
 
-    // console.log(circleArea, rectangleArea);
-
+    // Sample Input:
+    const count = countWordOccurrences("I love typescript TypeScript", "typescript");
+    console.log(count);
 
 }
